fe26813827406e3aaf21e33a5b8f8824b97c5d73
DawnTurn <adebowaleademuyiwa12@gmail.com>
2023-09-15 18:00:15 +0100

    update navba

--- js/index.js
+++ js/index.js
@@ -3,41 +3,43 @@ const carouselContainer = document.querySelector(".carousel-container");
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
 
-let counter = 1;
-const size = wrapper[0].clientWidth;
+// let counter = 1;
+// const size = wrapper[0].clientWidth;
 
-carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
+// console.log(size)
 
-// nextBtn.addEventListener("click", () => {
-//   if (counter >= 6) return;
+// carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
+
+// // nextBtn.addEventListener("click", () => {
+// //   if (counter >= 6) return;
+// //   carouselContainer.style.transition = "transform 0.3s ease-in-out";
+// //   counter++;
+// //   carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
+// // });
+
+// // prevBtn.addEventListener("click", () => {
+// //   if (counter <= 0) return;
+// //   carouselContainer.style.transition = "transform 0.3s ease-in-out";
+// //   counter--;
+// //   carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
+// // });
+
+// carouselContainer.addEventListener("transitionend", () => {
+//   if (wrapper[counter].id === "lastClone") {
+//     carouselContainer.style.transition = "none";
+//     counter = wrapper.length - 2;
+//     carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
+//   }
+
+//   if (wrapper[counter].id === "firstClone") {
+//     carouselContainer.style.transition = "none";
+//     counter = wrapper.length - counter;
+//     carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
+//   }
+// });
+
+// setInterval(() => {
 //   carouselContainer.style.transition = "transform 0.3s ease-in-out";
 //   counter++;
 //   carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
-// });
-
-// prevBtn.addEventListener("click", () => {
-//   if (counter <= 0) return;
-//   carouselContainer.style.transition = "transform 0.3s ease-in-out";
-//   counter--;
-//   carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
-// });
-
-carouselContainer.addEventListener("transitionend", () => {
-  if (wrapper[counter].id === "lastClone") {
-    carouselContainer.style.transition = "none";
-    counter = wrapper.length - 2;
-    carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
-  }
-
-  if (wrapper[counter].id === "firstClone") {
-    carouselContainer.style.transition = "none";
-    counter = wrapper.length - counter;
-    carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
-  }
-});
-
-setInterval(() => {
-  carouselContainer.style.transition = "transform 0.3s ease-in-out";
-  counter++;
-  carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
-}, 9000);
+// }, 9000);
