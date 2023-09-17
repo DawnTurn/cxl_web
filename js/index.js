988e070b4db3ed0a3c42b60a5725a4eb2f911afc
DawnTurn <adebowaleademuyiwa12@gmail.com>
2023-09-17 18:54:07 +0100

    add testimonial page

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ const wrapper = document.querySelectorAll(".wrapper");
 const carouselContainer = document.querySelector(".carousel-container");
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
+const testimonialCarousel = document.querySelector('.testimonial-carousel')
+const testimonial = document.querySelectorAll('.testimonial')
+const arrowRight = document.querySelector('.bx-chevron-right')
+const arrowLeft = document.querySelector('.bx-chevron-left')
 
 let counter = 1;
 const size = wrapper[0].clientWidth;
@@ -43,3 +47,39 @@ setInterval(() => {
   counter++;
   carouselContainer.style.transform = "translateX(" + -size * counter + "px)";
 }, 9000);
+
+
+// testimonials carousel
+
+let testCount = 1;
+const testSize = testimonial[0].clientWidth;
+
+testimonialCarousel.style.transform = "translateX(" + -testSize * testCount + "px)";
+
+arrowRight.addEventListener("click", () => {
+  if (testCount >= 5) return;
+  testimonialCarousel.style.transition = "transform 0.3s ease-in-out";
+  testCount++;
+  testimonialCarousel.style.transform = "translateX(" + -testSize * testCount + "px)";
+});
+
+arrowLeft.addEventListener("click", () => {
+  if (testCount <= 0) return;
+  testimonialCarousel.style.transition = "transform 0.3s ease-in-out";
+  testCount--;
+  testimonialCarousel.style.transform = "translateX(" + -testSize * testCount + "px)";
+});
+
+testimonialCarousel.addEventListener("transitionend", () => {
+  if (testimonial[testCount].id === "l-clone") {
+    testimonialCarousel.style.transition = "none";
+    testCount = testimonial.length - 2;
+    testimonialCarousel.style.transform = "translateX(" + -testSize * testCount + "px)";
+  }
+
+  if (testimonial[testCount].id === "f-clone") {
+    testimonialCarousel.style.transition = "none";
+    testCount = testimonial.length - testCount;
+    testimonialCarousel.style.transform = "translateX(" + -testSize * testCount + "px)";
+  }
+});
